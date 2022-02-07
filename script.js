aa05e70402de8c891518d07acefaef8a06bd3590
master <tohirovalisher18@gmail.com>
2022-02-07 11:21:14 +0500

    Loop operation

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Alisher Tohirov");
+// Tsikl operatori
+"use strict";
+
+// let x = 0;
+
+// while(x < 10){
+//     console.log(x);
+//     x++;
+// }
+
+
+// let x = 2;
+// do {
+//     console.log(x);
+//     x++;
+// }while(x < 10);
+
+// for(let i=0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// for(let i=0; i <=10; i++){
+//     if(i == 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+for(let i=0; i <= 10; i++){
+    if(i == 5){
+        continue;
+    }
+    console.log(i);
+}
